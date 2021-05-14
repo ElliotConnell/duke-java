f51f91fc760de7ef01a775cf6ca0deea8b946619
Elliot Connell <elliot.connell@gmail.com>
2021-05-14 10:45:12 +1000

    replaced testGray() method with selectAndConvert() method. allows users to convert multiple images

--- GrayScale/GrayScaleConvertor.java
+++ GrayScale/GrayScaleConvertor.java
@@ -7,6 +7,7 @@
  */
 
 import edu.duke.*;
+import java.io.*;
 
 public class GrayScaleConvertor {
     
@@ -34,10 +35,14 @@ public class GrayScaleConvertor {
             
     }
     
-    public void testGray() {
-        ImageResource ir = new ImageResource();
-        ImageResource gray = makeGray(ir);
-        gray.draw();
+    
+    public void selectAndConvert() {
+        DirectoryResource dr = new DirectoryResource();
+        for (File f: dr.selectedFiles()) {
+            ImageResource inImage = new ImageResource(f);
+            ImageResource gray = makeGray(inImage);
+            gray.draw();
+        }
     }
 
 }
