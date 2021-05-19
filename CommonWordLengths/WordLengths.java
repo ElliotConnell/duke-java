e366416d34829f4fd56d75284029c32027d364b5
Elliot Connell <elliot.connell@gmail.com>
2021-05-19 14:46:38 +1000

    WordLengths first commit

---
+++ CommonWordLengths/WordLengths.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of WordLengths here.
+ * You will write a program to figure out the most common word length of words from a file. 
+ * To solve this problem you will need to keep track of how many words from a file are of each possible length.
+ * You should group all words of length 30 or more together, 
+ * and you should not count basic punctuation that are the first or last characters of a group of characters.
+ * 
+ * @author (Elliot Connell) 
+ * @version (18/05/21)
+ */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+import java.lang.*;
+
+public class WordLengths {
+    
+    public void countWordLengths( FileResource resource, int [] counts) {
+        
+    }
+
+}
