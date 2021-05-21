cf59e9f2987df816cf55621f3cc7f81411311a55
Elliot Connell <elliot.connell@gmail.com>
2021-05-21 14:38:41 +1000

    created decryptTwoKeys() and testDecryptTwoKeys() methods

--- CaesarCipher/CaesarBreaker.java
+++ CaesarCipher/CaesarBreaker.java
@@ -5,6 +5,12 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+import java.lang.*;
+
 public class CaesarBreaker {
     
     public int[] countLetters(String message) {
@@ -80,11 +86,46 @@ public class CaesarBreaker {
     }
     
     public int getKey(String s) {
-        
+        // call countLetters to get an array of the letter frequencies in string s
         int[] counts = countLetters(s);
+        // calculate the index of the largest letter fequency
         int index = maxIndex(counts);
         
+        // return the index        
         return index;
+    }
+    
+    public String decryptTwoKeys(String encrypted){
+        // calculate a string of every other character starting with the first character of encrypted string
+        String stringA = halfOfString(encrypted, 0);
+        // calculate a string of every other character starting with the second character of encrypted string
+        String stringB = halfOfString(encrypted, 1);
+        // calculate the key used to encrypt each half string
+        int index1 = getKey(stringA);
+        int dkey1 = index1 - 4;
+        if (index1 < 4) {
+            dkey1 = 26 - (4-index1);
+        }
+        int index2 = getKey(stringB);
+        int dkey2 = index2 - 4;
+        if (index2 < 4) {
+            dkey2 = 26 - (4-index2);
+        }
+        // print out the two keys found
+        System.out.println("the two keys for the encrypted string are = " + dkey1 + " & " + dkey2);
+        
+        // calc and return the decrypted String using encryptTwoKeys method
+        CaesarCipher cc = new CaesarCipher();
+        String decrypted = cc.encryptTwoKeys(encrypted, 26 - dkey1, 26 - dkey2);
+        
+        return decrypted;        
+    }
+    
+    public void testDecryptTwoKeys(){
+        FileResource fr = new FileResource();
+        String message = fr.asString();
+        String result = decryptTwoKeys(message);
+        System.out.println(result);
     }
 
 }
