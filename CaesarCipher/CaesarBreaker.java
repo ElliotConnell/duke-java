35fcfa804ef215f6fed119e4528354e97a85cedb
Elliot Connell <elliot.connell@gmail.com>
2021-05-21 11:44:21 +1000

    CaesarBreaker first commit

---
+++ CaesarCipher/CaesarBreaker.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of CaesarBreaker here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CaesarBreaker {
+    
+    public int countLetters() {
+        
+        return 0;    
+    }
+    
+    public int maxIndex() {
+        return 0;
+        
+    }
+    
+    public String decrypt(String encrypted) {
+        CaesarCipher cc = new CaesarCipher();
+        
+        return "";
+    }
+
+}
