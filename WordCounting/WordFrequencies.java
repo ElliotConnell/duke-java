d4d9e3490df1b263d36cc39846685be88c2327e5
Elliot Connell <elliot.connell@gmail.com>
2021-05-27 18:29:56 +1000

    WordFrequencies first commit

---
+++ WordCounting/WordFrequencies.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of WordFrequencies here.
+ * You will write a program to determine the word that occurs the most often in a file. 
+ * If more than one word occurs as the most often, then return the first such word found. 
+ * You should make all words lowercase before counting them. Thus, “This” and “this” will
+ * both be counted as the lowercase version of “this”. You should not consider punctuation,
+ * so “end” and “end,” will be considered different words. Use the WordFrequencies program
+ * in the lesson as a starting point.
+ * 
+ * @author (Elliot Connell) 
+ * @version (27/05/21)
+ */
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class WordFrequencies {
+    
+    private ArrayList<String> myWords;
+    private ArrayList<Integer> myFreqs;
+
+}
