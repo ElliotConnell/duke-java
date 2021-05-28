47742f2433df961e08b9d39c4887a198cbc86a4d
Elliot Connell <elliot.connell@gmail.com>
2021-05-28 11:02:35 +1000

    added code for findIndexOfMax(). modified tester() to call findIndexOfMax()

--- WordCounting/WordFrequencies.java
+++ WordCounting/WordFrequencies.java
@@ -47,12 +47,30 @@ public class WordFrequencies {
         }
     }
     
+    public int findIndexOfMax(){
+        int index = 0;
+        int freq = 0;
+        
+        for (int k=0; k < myWords.size(); k++) {
+            if (myFreqs.get(k) > freq) {
+                freq = myFreqs.get(k);
+                index = k;
+            }
+        }
+        
+        return index;
+    }
+    
     public void tester() {
         findUnique();
         System.out.println("# unique words: " + myWords.size());
         for (int k=0; k < myWords.size(); k++){
             System.out.println(myFreqs.get(k)+"\t"+myWords.get(k));
         }
+        
+        int result = findIndexOfMax();
+        System.out.println("the word that occurs most often and its count are: " + myWords.get(result) + " " + myFreqs.get(result));
+        
     }
 
 }
