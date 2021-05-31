a62d2b1b07eed0c91644cb33527955880ecc86dc
Elliot Connell <elliot.connell@gmail.com>
2021-05-31 11:45:10 +1000

    added new ArrayLists fruitList and verbList

--- GladLib/GladLib.java
+++ GladLib/GladLib.java
@@ -9,6 +9,9 @@ public class GladLib {
 	private ArrayList<String> nameList;
 	private ArrayList<String> animalList;
 	private ArrayList<String> timeList;
+	private ArrayList<String> verbList;
+	private ArrayList<String> fruitList;
+	
 	
 	private Random myRandom;
 	
