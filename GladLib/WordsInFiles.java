1bb93ff09092372c613609957b064d979a1052ac
Elliot Connell <elliot.connell@gmail.com>
2021-06-02 14:18:36 +1000

    added code to addWordsFromFile

--- GladLib/WordsInFiles.java
+++ GladLib/WordsInFiles.java
@@ -10,9 +10,37 @@
  */
 import edu.duke.*;
 import java.util.*;
+import java.io.*; 
 
 public class WordsInFiles {
     
+    private HashMap<String, ArrayList<String>> fileMap;
+    
+    public WordsInFiles(){
+        fileMap = new HashMap<String, ArrayList<String>>();
+    }
+    
+    private void addWordsFromFile(File f){
+        FileResource fr = new FileResource(f);
+        ArrayList<String> filename = new ArrayList<String>();
+        
+        for (String sKey: fr.words()){
+            filename.clear();
+            //System.out.println(sKey);
+            
+            if (fileMap.containsKey(sKey)) {
+                filename = fileMap.get(sKey);
+                if (!filename.contains(f.getName())) {
+                    filename.add(f.getName());
+                }
+            }
+            else {
+                filename.add(f.getName());
+            }
+            
+            fileMap.put(sKey, filename);
+        }
+    }
     
 
 }
