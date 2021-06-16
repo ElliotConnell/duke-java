5ec33fd02c823cf1100840f922cb3b717cfd73e3
Elliot Connell <elliot.connell@gmail.com>
2021-06-16 11:22:01 +1000

    WebLogProgram first commit. original code supplied by Duke Universit y course resources

---
+++ WebLogProgram/LogAnalyzer.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class LogAnalyzer here.
+ * 
+ * The Class LogAnalyzer, which has been started for you.
+ * 
+ * Duke University Course resource
+ * 
+ * @author Duke University 
+ * @version 16/06/21
+ */
+
+import java.util.*;
+import edu.duke.*;
+
+public class LogAnalyzer
+{
+     private ArrayList<LogEntry> records;
+     
+     public LogAnalyzer() {
+         // complete constructor
+     }
+        
+     public void readFile(String filename) {
+         // complete method
+     }
+        
+     public void printAll() {
+         for (LogEntry le : records) {
+             System.out.println(le);
+         }
+     }
+     
+     
+}
---
+++ WebLogProgram/LogEntry.java
@@ -0,0 +1,52 @@
+
+/**
+ * Write a description of class LogRecord here.
+ * 
+ * The class LogEntry from the lesson that has five private variables, a constructor, five get methods 
+ * to access the private fields, and a toString method for printing out a LogEntry.
+ * 
+ * Duke University Course resource
+ * 
+ * @author Duke University 
+ * @version 16/06/21
+ */
+
+import  java.util.*;
+public class LogEntry
+{
+     private String ipAddress;
+     private Date accessTime;
+     private String request;
+     private int statusCode;
+     private int bytesReturned;
+     
+   public LogEntry(String ip, Date time, String req, int status, int bytes) {
+       ipAddress = ip;
+       accessTime = time;
+       request = req;
+       statusCode = status;
+       bytesReturned = bytes;
+       
+   }
+   
+   public String getIpAddress() {
+         return ipAddress;
+    }
+    public Date getAccessTime() {
+         return accessTime;
+   }   
+   public String getRequest() {
+         return request;
+   }
+   public int getStatusCode() {
+         return statusCode;
+   }
+   public int getBytesReturned() {
+         return bytesReturned;
+   }
+   
+   public String toString() {
+       return ipAddress + " " + accessTime + " " + request 
+           + " " + statusCode + " " + bytesReturned;
+    }
+}
