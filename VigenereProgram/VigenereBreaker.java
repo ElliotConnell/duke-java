50cb09a5e8a449f2de33feb299057ecc55d976f6
Elliot Connell <elliot.connell@gmail.com>
2021-06-21 13:47:59 +1000

    created tester() method

--- VigenereProgram/VigenereBreaker.java
+++ VigenereProgram/VigenereBreaker.java
@@ -17,4 +17,42 @@ public class VigenereBreaker {
         //WRITE YOUR CODE HERE
     }
     
+    public void tester(){
+        String result = sliceString("abcdefghijklm", 0, 3);
+        System.out.println("expected result = 'adgjm'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 1, 3);
+        System.out.println("expected result = 'behk'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 2, 3);
+        System.out.println("expected result = 'cfil'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 0, 4);
+        System.out.println("expected result = 'aeim'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 1, 4);
+        System.out.println("expected result = 'bfj'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 2, 4);
+        System.out.println("expected result = 'cgk'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 3, 4);
+        System.out.println("expected result = 'dhl'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 0, 5);
+        System.out.println("expected result = 'afk'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 1, 5);
+        System.out.println("expected result = 'bgl'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 2, 5);
+        System.out.println("expected result = 'chm'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 3, 5);
+        System.out.println("expected result = 'di'. returns " + result);
+        
+        result = sliceString("abcdefghijklm", 4, 5);
+        System.out.println("expected result = 'ej'. returns " + result);
+    }
+    
 }
